1ee92a172ebe76698c6ec2c8a0e3c273546844c6
Hamis Badarou <hamis@poiscaille.fr>
2022-09-13 20:23:25 +0000

    POISCAILLE_PIPELINE_20220913

--- models/1.reporting/2.Growth/rep_pr_global_mongo.sql
+++ models/1.reporting/2.Growth/rep_pr_global_mongo.sql
@@ -5,66 +5,104 @@
    )
 }}
 
-
-WITH place_data AS (
-  SELECT place_id,
-        place_name, 
-        place_city , 
-        place_address,
-        CAST(place_createdat AS DATE) AS DATE_CREATION
-        FROM {{ ref('stg_mongo_place_consolidation') }} 
-),
-
-sale_data AS (
-SELECT 
-'Point Relais' as company_type, 
-place_name, 
-place_id,
-place_city , 
-place_address,  
-place_email, 
-place_codepostal, 
-place_phone , 
-CAST(place_createdat AS DATE) AS created_date,
--- code promos associé au PR
--- nombre de fois où le code promo a été utilisé 
--- methode de livraison , chrono - chronofresh sinon stripe 
--- jour et heure de livraison
+with place_consolidation as (
+select
+place_id, 
 COUNT (DISTINCT(case when type_sale = 'abonnement' then user_id end)) AS nb_subscribers,
-sum(case when type_sale = 'abonnement' then price_ttc end ) as total_subscriptions,
-sum(case when type_sale = 'shop' then price_ttc end ) as total_shop,
-sum(case when type_sale = 'Petit plus' then price_ttc end ) as total_petitplus,
+sum(case when type_sale = 'abonnement' then price_ttc end ) as total_ca_subscriptions,
+sum(case when type_sale = 'shop' then price_ttc end ) as total_ca_shop,
+sum(case when type_sale = 'Petit plus' then price_ttc end ) as total_ca_petitplus,
 COUNT (DISTINCT(case when type_sale = 'Petit plus' then sale_id end)) AS nb_vente_petitplus,
 COUNT (DISTINCT(case when type_sale = 'Petit plus' or type_sale = 'shop'  then sale_id end)) AS nb_vente_hors_abo,
-SUM(price_ttc) AS CA 
+SUM(price_ttc) AS total_ca
 -- montant des remboursements
 -- Panier Moyen hors ca
 FROM {{ ref('stg_mongo_sale_consolidation') }} 
 WHERE place_id IS NOT NULL
-GROUP BY place_id,
-place_name, 
-place_city , 
-place_address,
-DATE_CREATION
+GROUP BY 1
+) , 
+place_info as (
+
+select 
+distinct 
+  place_id,
+  place_name,
+  place_owner,
+  place_phone,
+  place_city,
+  place_address,
+  place_codepostal,
+  place_email,
+  place_coupon,
+  place_lng,
+  place_lat,
+  place_geocode,
+  place_createdat,
+  shipping_pickup,
+  shipping_delay,
+  place_company,
+  place_coupon_users,
+  place_coupon_amount,
+  shipping_company,
+  days_since_in_bdd,
+  months_since_in_bdd,
+  year_since_in_bdd,
+  type_livraison,
+  place_storage,
+  place_icebox,
+  place_pickup,
+  place_openings_schedule,
+  place_openings_hidden,
+  place_openings_day,
+  place_openings_depositschedule,
+  nom_departement,
+  nom_region,
+  zone, 
+  case when nom_region = 'Île-de-France' then 'IDF' else 'Région' end as place_location
+  FROM {{ ref('stg_mongo_sale_consolidation') }} 
+
 )
 
-SELECT 
-        'Point Relais' as company_type, 
-        place_data.place_id,
-        place_data.place_name, 
-        place_data.place_city , 
-        place_data.place_address,
-        place_data.DATE_CREATION,
-        nb_subscribers,
-        total_subscriptions,
-        total_shop,
-        total_petitplus,
-        nb_vente_petitplus,
-        nb_vente_hors_abo,
-        CA
-        FROM place_data
-        LEFT JOIN sale_data ON place_data.place_id = sale_data.place_id
-
+select 
+   place_consolidation.*,
+  place_name,
+  place_owner,
+  place_phone,
+  place_city,
+  place_address,
+  place_codepostal,
+  place_email,
+  place_coupon,
+  place_lng,
+  place_lat,
+  place_geocode,
+  place_createdat,
+  shipping_pickup,
+  shipping_delay,
+  place_company,
+  place_coupon_users,
+  place_coupon_amount,
+  shipping_company,
+  days_since_in_bdd,
+  months_since_in_bdd,
+  year_since_in_bdd,
+  type_livraison,
+  place_storage,
+  place_icebox,
+  place_pickup,
+  place_openings_schedule,
+  place_openings_hidden,
+  place_openings_day,
+  place_openings_depositschedule,
+  nom_departement,
+  nom_region,
+  zone,
+  place_location
+  from place_consolidation
+  left join place_info
+  on place_consolidation.place_id = place_info.place_id 
+  order by place_consolidation.place_id asc 
+  
 
 
 
