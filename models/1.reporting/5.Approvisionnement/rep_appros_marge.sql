d142e3d8f6d165edf785c6f9b568f3ac2dda68c6
Hamis Badarou <hamis@poiscaille.fr>
2022-10-17 17:28:56 +0000

    offerings_value_count

--- models/1.reporting/5.Approvisionnement/rep_appros_marge.sql
+++ models/1.reporting/5.Approvisionnement/rep_appros_marge.sql
@@ -14,7 +14,7 @@ select
 
   max(sale_price_ttc) as sale_price_ttc,
   sum(total_items_value_allocations_value_cost_ttc) as total_cost_ttc,
-  round((max(sale_price_ttc) - sum(total_items_value_allocations_value_cost_ttc))/max(sale_price_ttc),2) as gross_margin
+  --round((max(sale_price_ttc) - sum(total_items_value_allocations_value_cost_ttc))/max(sale_price_ttc),2) as gross_margin
 
 from {{ ref('rep_appros') }}
 group by 1,2,3,4,5
