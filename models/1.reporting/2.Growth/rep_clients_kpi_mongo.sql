e5481b15d2f479c160187c83933ae53daa8780de
Hamis Badarou <hamis@poiscaille.fr>
2022-11-18 12:10:35 +0000

    update date prochain casier

--- models/1.reporting/2.Growth/rep_clients_kpi_mongo.sql
+++ models/1.reporting/2.Growth/rep_clients_kpi_mongo.sql
@@ -357,7 +357,15 @@ result as (
         end as place_openings_day_preparation, 
         ca_global_stripe - refund_global_stripe as reel_ca_global_stripe,
     
-    case when place_name = 'Livraison à domicile' then 'Domicile' else 'Point Relais' end as  place_type
+    case when place_name = 'Livraison à domicile' then 'Domicile' else 'Point Relais' end as  place_type,
+
+    -- key dates
+    date_add(next_locker_date, interval 1 day) as next_locker_preparation_date,
+    case
+      when place_name <> 'Livraison à domicile' and localisation = 'Ile-de-France' then date_add(next_locker_date, interval 1 day) -- Point relais
+      when place_name = 'Livraison à domicile' and localisation = 'Ile-de-France' then date_add(next_locker_date, interval 2 day) -- Domicile
+      when localisation <> 'Ile-de-France' then date_add(next_locker_date, interval 2 day)
+    end as next_locker_shipping_date,
   
   FROM user_data 
   LEFT JOIN subscription_final_data ON user_data.user_id = subscription_final_data.user_id_subscription
