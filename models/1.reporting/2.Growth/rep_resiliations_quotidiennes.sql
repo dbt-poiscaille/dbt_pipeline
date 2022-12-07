631cc6d250d7b355913b15245efa111f9c0936d3
Hamis Badarou <hamis@poiscaille.fr>
2022-12-07 18:08:52 +0000

    Update Pipeline Remboursement et Resiliations

---
+++ models/1.reporting/2.Growth/rep_resiliations_quotidiennes.sql
@@ -0,0 +1,14 @@
+{{
+    config(
+        materialized='table',
+        labels={'type': 'mongodb', 'contains_pie': 'yes', 'category': 'production'},
+    )
+}}
+
+select
+    cast(unsubscribed.at as date) as unsubscribed_date,
+    unsubscribed.reason,
+    unsubscribed.detailedreason,
+    concat('https://poiscaille.fr/kraken/client/', user._id) as link
+from {{ source('mongodb', 'subscription') }}
+where unsubscribed.at is not null
